f45ad34039a6b28d8df4181cfe3ad9ec640ba4f4
Bryan Sfara <bryansfara@gmail.com>
2018-06-25 15:50:18 -0400

    Added font color white

--- client/message_padding.py
+++ client/message_padding.py
@@ -4,7 +4,7 @@ import os
 
 PLUGIN_PATH = os.path.realpath(os.path.dirname(__file__))
 STATIC_PADDING = """\
-<p style=\"font-size: 0px\">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. \
+<p style=\"font-size: 0px\font-color:#000000;">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. \
 The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it \
 look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem \
 ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose. \
